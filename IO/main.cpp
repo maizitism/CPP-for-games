03359b58de3f334dbdb25d349cbf9764a4a3d0c4
Marks Maizītis <mmaizitis123@gmail.com>
2025-11-29 14:49:38 +0100

    Add hardcoded name and age variables to IO/main.cpp

--- IO/main.cpp
+++ IO/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main() {
+	std::string name = "Marks";
+	int age = 21;
+
+	std::cout << "Hello, " << name << ". ";
+	std::cout << "You are " << age << " years old!" << std::endl;
 
 	return 0;
 }
