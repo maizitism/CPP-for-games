66bafb7d6dca35c70a025dcd00800400149a5321
Marks Maizītis <mmaizitis123@gmail.com>
2025-11-29 13:36:32 +0100

    Create main.cpp and Config.hpp for showcasing headers

---
+++ Build Process/Config.hpp
@@ -0,0 +1,5 @@
+#pragma once // makes sure that if file is included more then once, preprocessor doesnt include it again
+
+// adding variables in global scope is bad practice
+float jumpHeight = 50.0f;
+float jumpDuration = 0.5f;
---
+++ Build Process/main.cpp
@@ -0,0 +1,10 @@
+#include "Config.hpp"
+#include <iostream>
+// quotes for including local files in project, <> for including system or library headers
+
+
+int main() {
+	std::cout << "jumpHeight: " << jumpHeight << std::endl;
+	std::cout << "jumpDuration: " << jumpDuration << std::endl;
+	return 0;
+}
