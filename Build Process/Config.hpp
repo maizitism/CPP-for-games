5a841477551bb5eb3a3f3233344458d31fc9550b
Marks Maizītis <mmaizitis123@gmail.com>
2025-11-29 13:49:15 +0100

    add declarative variables to config.hpp

--- Build Process/Config.hpp
+++ Build Process/Config.hpp
@@ -1,7 +1,12 @@
 #pragma once // makes sure that if file is included more then once, preprocessor doesnt include it again
 
 // adding variables in global scope is bad practice
-float jumpHeight = 50.0f;
-float jumpDuration = 0.5f;
+//float jumpHeight = 50.0f;
+//float jumpDuration = 0.5f;
 // These are define variables - storage is being allocated for these varioables in memory
 // Variables will be stored in main.obj file.
+
+// declaration variables
+// telling linker that these variables exist somewhere, but not here.
+extern float jumpHeight;
+extern float jumpDuration;
