e1f7b1eea9b12d8ebf9a3c016ee86dbe70efe07d
Marks Maizītis <mmaizitis123@gmail.com>
2025-11-29 13:41:39 +0100

    Add explanatory comment for variable definition

--- Build Process/Config.hpp
+++ Build Process/Config.hpp
@@ -3,3 +3,5 @@
 // adding variables in global scope is bad practice
 float jumpHeight = 50.0f;
 float jumpDuration = 0.5f;
+// These are define variables - storage is being allocated for these varioables in memory
+// Variables will be stored in main.obj file.
