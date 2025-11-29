ff6588cca95fbe7b7383e4ba5f86c7ef13cb2fb5
Marks Maizītis <mmaizitis123@gmail.com>
2025-11-29 14:59:07 +0100

    Add wrong input checking for input of age

--- IO/main.cpp
+++ IO/main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <string>
+#include <limits>
 
 int main() {
 	std::string name;
 	int age;
 
-	std::cout << "What is your name?: ";
+	std::cout << "What is your name?: " << std::endl;
 	std::cin >> name;
 
-	std::cout << "\nWhat is your age?: ";
+	std::cout << "What is your age?: " << std::endl;
 	std::cin >> age;
 	
+	// if user doesnt input number for age, failstate will be set for cin.
+	while (std::cin.fail()) {
+		std::cout << "Error! An number was expected when prompted for your age." << std::endl;
+		std::cin.clear(); // clear the error state
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore all characters in current input stream until the newline character
+		std::cin >> age; // prompt user for age again
+	}
 
 	std::cout << "Hello, " << name << ". ";
 	std::cout << "You are " << age << " years old!" << std::endl;
