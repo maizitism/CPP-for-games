db55ee319d8cd7b31338a857007f09b3c11f42ad
Marks Maizītis <mmaizitis123@gmail.com>
2025-11-29 15:06:55 +0100

    Add own "press enter to exit" prompt

--- IO/main.cpp
+++ IO/main.cpp
@@ -14,7 +14,7 @@ int main() {
 	
 	// if user doesnt input number for age, failstate will be set for cin.
 	while (std::cin.fail()) {
-		std::cout << "Error! An number was expected when prompted for your age." << std::endl;
+		std::cout << "Error! An number was expected when prompted for your age.\nInput your age again." << std::endl;
 		std::cin.clear(); // clear the error state
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore all characters in current input stream until the newline character
 		std::cin >> age; // prompt user for age again
@@ -22,6 +22,12 @@ int main() {
 
 	std::cout << "Hello, " << name << ". ";
 	std::cout << "You are " << age << " years old!" << std::endl;
+
+	std::cout << "Press enter to exit." << std::endl;
+	// when enabling "Automatically Close Console When Debugging Stops", there might still be a couple characters
+	// in the input stream, which will get consumed by std::cin.get().
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cin.get();
 
 	return 0;
 }
