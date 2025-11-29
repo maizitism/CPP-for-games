ab924ad737cb309f5e6a92ce4a9b9411c47cc0ad
Marks Maizītis <mmaizitis123@gmail.com>
2025-11-29 15:39:48 +0100

    Verify that inputted age is a number

--- IO Lab Assignment/main.cpp
+++ IO Lab Assignment/main.cpp
@@ -13,11 +13,19 @@ int main() {
 	std::cout << "What is your age?" << std::endl;
 	std::cin >> age;
 
+	// if user doesnt input number for age, failstate will be set for cin.
+	while (std::cin.fail()) {
+		std::cout << "Error! An number was expected when prompted for your age.\nInput your age again." << std::endl;
+		std::cin.clear(); // clear the error state
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore all characters in current input stream until the newline character
+		std::cin >> age; // prompt user for age again
+	}
+
 	if (age > 0 && age < 25) {
-		std::cout << "Hello " << name << ",you are only " << age << "years old.";
+		std::cout << "Hello " << name << ", you are only " << age << "years old.";
 	}
 	else if (age >= 25 && age < 50) {
-		std::cout << "Hello " << name << ",you are already " << age << "years old.";
+		std::cout << "Hello " << name << ", you are already " << age << "years old.";
 	}
 	else if (age >= 50) {
 		std::cout << "Hello " << name << ". You are very wise for your age.";
