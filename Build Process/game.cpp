8a3f6faebf46b380df43445bd3f58b1d5e87235c
Marks Maizītis <mmaizitis123@gmail.com>
2025-11-29 13:44:33 +0100

    add logic to game.cpp

---
+++ Build Process/game.cpp
@@ -0,0 +1,8 @@
+#include "Config.hpp"
+
+#include <iostream>
+
+void Update() { // function is of return type void, no return variable needed
+	std::cout << "jumpHeight: " << jumpHeight << std::endl;
+	std::cout << "jumpDuration: " << jumpDuration << std::endl;
+}
