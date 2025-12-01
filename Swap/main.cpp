c6ea6ddb51c089ccc6a7a106660b257e34bcada8
Marks Maizītis <mmaizitis123@gmail.com>
2025-12-01 16:23:40 +0100

    Fix errors in logic when swapping by pointer copy

--- Swap/main.cpp
+++ Swap/main.cpp
@@ -3,9 +3,13 @@
 void swap(int* a, int* b) {
 	// swap the value of a and b around
 	// so a == initial_b and b == initial_a
-	int* temp = b;
-	b = a;
-	a = temp;
+	
+	// were dereferencing the COPIES of the pointers given,
+	// then changing their values around, to affect change in int main()
+
+	int temp = *b;
+	*b = *a;
+	*a = temp;
 }
 
 int main() {
@@ -15,7 +19,7 @@ int main() {
 	std::cout << "The initial values of a and b are: " << a << ", " 
 		<< b << std::endl;
 	swap(&a, &b);
-	std::cout << "After swapping a and b by pointer, the values are" 
+	std::cout << "After swapping a and b by pointer, the values are: " 
 		<< a << ", " << b << std::endl;
 	// call swap function
 	// print values to console
