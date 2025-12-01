c0de1165adab6de0ea1730795faf7f65dd330d48
Marks Maizītis <mmaizitis123@gmail.com>
2025-12-01 17:45:19 +0100

    Add implementation of computeMaximum/Minimum/Average and array allocation

--- Dynamic Array/main.cpp
+++ Dynamic Array/main.cpp
@@ -1,4 +1,34 @@
 #include <iostream>
+#include <limits>
+#include <climits>
+
+float computeMaximum(int* arr, int size) {
+	float max = arr[0];
+	for (int i = 0; i <= size; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+float computeMinimum(int* arr, int size) {
+	float min = arr[0];
+	for (int i = 0; i <= size; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+float computeAverage(int* arr, int size) {
+	float sum = 0;
+	for (int i = 0; i <= size; i++) {
+		sum += arr[i];
+	}
+	return sum/size;
+}
 
 int main() {
 	enum class AllocatorState {
@@ -18,9 +48,11 @@ int main() {
 			state = AllocatorState::CinFail;
 			std::cout << "Input number is not an integer.\nTry again."
 				<< std::endl;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			continue;
 		}
-		if (size <= 0 && size > 10) {
+		if (size <= 0 || size > 10) {
 			state = AllocatorState::SizeFail;
 			std::cout << "Allocated size must be greater then 0 and"
 				<< " smaller then or equal to 10. Try again." << std::endl;
@@ -31,6 +63,34 @@ int main() {
 		state = AllocatorState::Good;
 	}
 	
+	int* arr = new int[size] {};
+	// now, user has to fill the allocated int array with some numbers
+	int input = 0;
+	for (int i = 0; i < size; i++) {
+		std::cout << "Enter the " << i << "th value of the array!" << std::endl;
+		std::cin >> input;
+		// check if inputted number is valid
+		while (std::cin.fail()) {
+			std::cout << "Inputted number is not valid. Try again." << std::endl;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin >> input;
+		}
+		arr[i] = input;
+	}
 
+	int max = computeMaximum(arr, size);
+	int min = computeMinimum(arr, size);
+	int avg = computeMinimum(arr, size);
+
+	std::cout << "The maximum value of the array is: " << max << std::endl <<
+		"The minimum value of the array is: " << min << std::endl <<
+		"The average of the values in the array is: " << avg << std::endl;
+
+	//after were finished, array must be freed :smile:
+	delete[] arr;
+
+	// reflecting back on what i have wrote, this couldve been written way
+	// better with less copy paste, but eh what are you going to do
 	return 0;
 }
