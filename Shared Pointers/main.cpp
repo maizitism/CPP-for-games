46564ffdbf8081923225679fa3731e2205c5fe77
Marks Maizītis <mmaizitis123@gmail.com>
2025-12-25 20:09:50 +0100

    Rewatch and finish Shared Pointer lecture

--- Shared Pointers/main.cpp
+++ Shared Pointers/main.cpp
@@ -1,6 +1,73 @@
 #include <iostream>
+#include <memory>
+
+class Widget {
+public:
+	//create constructor
+	Widget(int data = 0) : data{ data } {
+		std::cout << "Widget:Widget( " << data << " )" << std::endl;
+	}
+	//create destructor
+	~Widget() {
+		std::cout << "~Widget::Widget()" << std::endl;
+	}
+
+	void doSomething() {
+		std::cout << "Do something." << std::endl;
+	}
+private:
+	//create member variable
+	int data = 0;
+};
+
+
+// lets suppose that our widget class needs a special deletor
+struct WidgetDeleter { // everything in a struct is public by default
+	void operator()(Widget* w) {
+		std::cout << "WidgetDeleter::operator()" << std::endl;
+		delete w; // the default deletor will do this automatically
+	}
+};
+
 
 int main() {
+	
+	// we can omit the shared pointer type and use auto as the type
+	// is already defined in the other side of the assignment
+	// auto widget = std::shared_ptr<Widget>(new Widget(1));
+
+	//with shared pointers, we no longer need to control when its deleted
+	// we dont need to use the new keyword
+
+	// a better method would be to use the make_shared function
+	auto widget = std::make_shared<Widget>(1); // preferred method
+	// since its a smart pointer, it gets automatically deleted when
+	// it goes out of scope
+
+	// we can allow multiple instances of this
+
+	std::cout << "Use count: " << widget.use_count() << std::endl;
+	auto widget2 = widget;
+	std::cout << "Use count: " << widget.use_count() << std::endl;
+	// widget isnt destroyed until both go out of scope
+
+	{
+		std::weak_ptr<Widget> wPtr;
+		wPtr = widget; // this will not increment the use count.
+		std::cout << "Use count: " << widget.use_count() << std::endl;
+
+		// for the weak pointer not to go out of scope, we can lock it
+		if (!wPtr.expired()) {
+			auto widget_weak_locked = wPtr.lock();
+			// this returns a shared pointer to the original widget
+		}
+	
+	}
+	
+	auto customWidget = std::shared_ptr<Widget>(new Widget(1), WidgetDeleter());
+	// we can give shared pointers custom deletors, but it has to be initialised
+	// "manually"
+
 
 	return 0;
 }
