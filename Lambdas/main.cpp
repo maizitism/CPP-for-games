a04279c89fb0848238e97f52da46523d887cfce5
Marks Maizītis <mmaizitis123@gmail.com>
2026-01-15 16:32:27 +0100

    Finish Lambdas Lab assignment

--- Lambdas/main.cpp
+++ Lambdas/main.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <functional>
+
+using add_func = std::function<int(int, int)>;
+
+
+int callFunc(add_func f, int a, int b) {
+	// with auto f, we dont quite know the type of f
+	// this way, it isnt explicit what type the function is
+	// we can either use a function pointer, making the func. signature
+	// callFunc(int(*f)(int, int))
+	// but this looks a little messy so we can typedef it and then add it 
+	// into the list of arguments.
+
+	return f(a, b);
+}
 
 
 int main() {
@@ -24,6 +39,10 @@ int main() {
 
 	// generic lambdas
 	auto h = [](auto a, auto b) { return a + b; };
+	//lambdas can also have explicit template parameters
+	//auto h = []<typename T> (T a, T b) { return a + b; };
+	// allows for forcing of types, where with auto they would not be guaranteed
+
 
 	auto i = h(5, 10);
 	auto j = h(5.0f, 10.0f);
