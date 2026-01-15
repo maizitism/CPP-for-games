411d4111f12a489d052918f0fc46f24480ce0e62
Marks Maizītis <mmaizitis123@gmail.com>
2026-01-15 19:19:15 +0100

    Finish "Explicit Constructors" lab

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -13,6 +13,11 @@ public: // make our constructor public, so an instance may be created.
 	// if a default value is given within the constructor, then the compiler will
 	// generate both a default constructor and a parameterised one
 
+	int getHitpoints() const { // since class is not modified and were only returning data, we can mark this as const
+		return hitPoints;
+	}
+
+
 private: // lets give the class some data
 	int hitPoints = 0; // we can also initialise them in the constructor
 
@@ -24,9 +29,23 @@ Entity::Entity(int hitPoint)
 	std::cout << "Entity::Entity(" << this->hitPoints << ")" << std::endl;
 }
 
+// lets define a function which will print these hitpoints
+void printHitpoints(const Entity& e) {
+	// we can only call const functions on the alias of this entity
+	std::cout << "hitPoints: " << e.getHitpoints() << std::endl;
+}
 
 int main() {
 	// now, lets create an instance of the class
 	Entity entity{50};
+	// lets print the hitpoints of that entity
+	printHitpoints(entity);
+	// we can abuse this function to lead to bugs
+	printHitpoints(10); // an another entity is being created here for whatever reason
+	// whats happening here is called an implicit conversion from an integer
+	// this happens when the function called is not marked as explicit
+
+	Entity entity2 = 100; // valid because we have a parameterised constructor which takes an int
+
 	return 0;
 }
